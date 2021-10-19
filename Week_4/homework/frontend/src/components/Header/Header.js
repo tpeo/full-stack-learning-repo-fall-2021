7bf9c9b754706c5e5cd801d5eaf5bf83f4a4ee80
Evan <lock.evan@gmail.com>
2021-10-18 21:43:57 -0500

    finish resume

--- Week_4/homework/frontend/src/components/Header/Header.js
+++ Week_4/homework/frontend/src/components/Header/Header.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 export default function Header() {
   return (
     <div className="header-container">
-      <h1 className="title-text">BLANK NAME's Todo App</h1>
+      <h1 className="title-text">Evan's Todo App</h1>
     </div>
   );
 }
